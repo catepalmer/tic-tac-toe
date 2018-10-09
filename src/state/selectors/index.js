8c6253e447faf2f7d3c7d1083892115fa400b7fa
Cate Palmer <catherinepalmer@gmail.com>
2018-10-09 17:32:22 +1300

    Add getWinningPlayer and getWinningSquares selectors

--- src/state/selectors/index.js
+++ src/state/selectors/index.js
@@ -2,3 +2,11 @@
 export function getMoves ({ moves }) {
   return moves
 }
+
+export function getWinningPlayer ({ winningPlayer }) {
+  return winningPlayer
+}
+
+export function getWinningSquares ({ winningSquares }) {
+  return winningSquares
+}
--- src/state/selectors/index.spec.js
+++ src/state/selectors/index.spec.js
@@ -1,5 +1,5 @@
 // src/state/selectors/index.spec.js
-import { getMoves } from '.'
+import { getMoves, getWinningPlayer, getWinningSquares } from '.'
 
 describe('state:selectors', () => {
   describe('getMoves', () => {
@@ -8,6 +8,24 @@ describe('state:selectors', () => {
       const state = { moves }
 
       expect(getMoves(state)).toBe(moves)
+    })
+  })
+
+  describe('getWinningPlayer', () => {
+    it('extracts the moves array from the state', () => {
+      const winningPlayer = 'x'
+      const state = { winningPlayer }
+
+      expect(getWinningPlayer(state)).toBe(winningPlayer)
+    })
+  })
+
+  describe('getWinningSquares', () => {
+    it('extracts the moves array from the state', () => {
+      const winningSquares = [0, 3, 6]
+      const state = { winningSquares }
+
+      expect(getWinningSquares(state)).toBe(winningSquares)
     })
   })
 })
